a6ef2269f7c0c3095a8551fc19f2aca2f11229b8
sprugit <spru.biz@gmail.com>
2023-02-24 16:30:01 +0000

    TP do servidor completa. TP da Cifra Incompleta. Upload do q tá feito do trabalho mais tarde

---
+++ TPs/ServidorCliente/src/aula1/myClient.java
@@ -0,0 +1,78 @@
+package aula1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class myClient {
+
+	public static void main(String[] args) {
+
+		String[] argv = { "0.0.0.0", "23456" };
+		Socket soc = null;
+		try {
+			soc = new Socket(argv[0], Integer.valueOf(argv[1]));
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		
+		try (ObjectInputStream in = new ObjectInputStream(soc.getInputStream());
+				ObjectOutputStream out = new ObjectOutputStream(soc.getOutputStream())) {
+			String user = args[0];
+			String pw = args[1];
+			out.writeObject((String) user);
+			out.writeObject((String) pw);
+			out.flush();
+
+			Boolean auth = (Boolean) in.readObject();
+
+			if (!auth) {
+				System.err.println("Autenticação falhou");
+				System.exit(1);
+			}
+
+			String fpath = args[2];
+			File f = new File(fpath);
+			if (!f.exists()) {
+				System.err.println("Uh oh ficheiro não existe");
+				System.exit(1);
+			}
+
+			long fsize = f.length();
+			String name = f.getName();
+
+			try (FileInputStream fis = new FileInputStream(f);) {
+
+				long totalSent = 0;
+
+				out.writeObject((String) name);
+				out.writeObject((long) fsize);
+
+				byte[] buffer = new byte[1024];
+
+				while (totalSent <= fsize) {
+					long read = fis.read(buffer, 0, buffer.length);
+					out.write(buffer, 0, (int) read);
+					out.flush();
+					totalSent += read;
+				}
+
+				System.out.println("Ficheiro enviado.");
+
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		} catch (IOException e) {
+			//e.getStackTrace();
+			System.err.println("Ligação Terminada pelo anfitrião remoto.");
+		} catch (ClassNotFoundException e1) {
+			//e1.printStackTrace();
+			System.err.println("Objeto recebido desconhecido.");
+		}
+
+	}
+
+}
---
+++ TPs/ServidorCliente/src/aula1/myServer.java
@@ -0,0 +1,115 @@
+/***************************************************************************
+*   Seguranca Informatica
+*
+*
+***************************************************************************/
+package aula1;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class myServer{
+
+	public static void main(String[] args) {
+		System.out.println("servidor: main");
+		myServer server = new myServer();
+		server.startServer();
+	}
+
+	public void startServer (){
+		ServerSocket sSoc = null;
+        
+		try {
+			sSoc = new ServerSocket(23456);
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+			System.exit(-1);
+		}
+		while(true) {
+			try {
+				Socket inSoc = sSoc.accept();
+				ServerThread newServerThread = new ServerThread(inSoc);
+				newServerThread.start();
+		    }
+		    catch (IOException e) {
+		        e.printStackTrace();
+		    }
+		}
+		//sSoc.close();
+	}
+
+
+	//Threads utilizadas para comunicacao com os clientes
+	class ServerThread extends Thread {
+
+		private Socket socket = null;
+
+		ServerThread(Socket inSoc) {
+			socket = inSoc;
+			System.out.println("thread do server para cada cliente");
+		}
+ 
+		public void run(){
+			try(ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());){
+				String user = null;
+				String passwd = null;
+			
+				try {
+					user = (String) inStream.readObject();
+					
+					passwd = (String) inStream.readObject();
+					
+					System.out.println("thread: depois de receber a password e o user");
+				}catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+ 			
+				//TODO: refazer
+				//este codigo apenas exemplifica a comunicacao entre o cliente e o servidor
+				//nao faz qualquer tipo de autenticacao
+				
+				if(!user.contentEquals("utilizador1") || !passwd.contentEquals("password2")) {
+					outStream.writeObject( (Boolean) false);
+					outStream.flush();
+				} else {
+					outStream.writeObject( (Boolean) true);
+					outStream.flush();
+				
+					String name = (String) inStream.readObject();
+					long fsize = (long) inStream.readObject();
+					
+					String path = "recebidos/";
+					
+					File f = new File(path+name);
+					try(FileOutputStream fos = new FileOutputStream(f);){
+						
+						long total = 0;
+						byte[] buffer = new byte[1024];
+						int toRead;
+						
+						while(total < fsize) {
+							toRead = (int) (fsize - total);
+							if(toRead > 1024) {
+								toRead = 1024;
+							} 
+							long received = inStream.read(buffer, 0, toRead);
+							fos.write(buffer, 0 , (int) received);
+							fos.flush();
+							total += received;
+						}
+					}
+					System.out.println("Ficheiro recebido com sucesso");
+				}
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
