2ea8c33940ac125e3412d0a8f95126aab5de477c
sprugit <spru.biz@gmail.com>
2023-02-26 18:05:01 +0000

    Exercicio de Cifras Simetricas, Assimetricas e Assinaturas Completo.

--- TPs/Cifras1/src/ex56/Assina.java
+++ TPs/Cifras1/src/ex56/Assina.java
@@ -15,22 +15,25 @@ import javax.crypto.NoSuchPaddingException;
 
 //https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#Signature
 public class Assina {
-	
+
 	private static final String input = "files/ex56/assinaturas/";
-	
+
 	private static byte[] genKey(File f) {
 		byte[] retval = null;
-		try(FileInputStream fis = new FileInputStream(f);){
-			Signature s = Signature.getInstance("NONEwithRSA");
+		try (FileInputStream fis = new FileInputStream(f);) {
+			//Signature s = Signature.getInstance("NONEwithRSA");
+			Signature s = Signature.getInstance("SHA256withRSA");
 			
-			//Assinatura usa chave privada para assinar
+			// Assinatura usa chave privada para assinar
 			s.initSign(AssymKeyHandler.getPrivateKey("keystore.chaves", "password", "chaves"));
-			
+
 			long read = 0;
 			byte[] buffer = new byte[1024];
-			while(read >= 0) {
+			while (read >= 0) {
 				read = fis.read(buffer, 0, buffer.length);
-				s.update(buffer);
+				if (read > -1) {
+					s.update(buffer, 0, (int) read);
+				}
 			}
 			retval = s.sign();
 		} catch (FileNotFoundException e) {
@@ -51,15 +54,15 @@ public class Assina {
 		}
 		return retval;
 	}
-	
+
 	private static void assina(String filename) {
-		
+
 		File target = new File(filename);
 		if (!target.exists() || target.isDirectory()) {
 			System.err.println("Ficheiro não existe.");
 			System.exit(1);
 		}
-		try(FileOutputStream fos = new FileOutputStream(input+filename+".assinatura");){
+		try (FileOutputStream fos = new FileOutputStream(input + filename + ".assinatura");) {
 			fos.write(genKey(target));
 			fos.flush();
 		} catch (FileNotFoundException e) {
@@ -68,42 +71,61 @@ public class Assina {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
 	}
 
-	//Verificação está errada I think, check later
-	private static boolean verifica(String filename) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, FileNotFoundException, IOException {
+	// https://www.tutorialspoint.com/java_cryptography/java_cryptography_verifying_signature.htm
+	private static boolean verifica(String filename) throws NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, FileNotFoundException, IOException, SignatureException {
 
-		File target = new File(input+filename);
+		File target = new File(filename);
 		if (!target.exists() || target.isDirectory()) {
 			System.err.println("Ficheiro não existe.");
 			System.exit(1);
 		}
-		
-		try(FileInputStream fin = new FileInputStream(target);){
-			String sig = Base64.getEncoder().encodeToString(fin.readAllBytes());
-			return sig.contentEquals(Base64.getEncoder().encodeToString(genKey(target)));
-		} 
+
+		try (FileInputStream fis = new FileInputStream(target);
+				FileInputStream sfis = new FileInputStream(input + filename + ".assinatura")) {
+			byte[] signature = sfis.readAllBytes();
+
+			Signature s = Signature.getInstance("SHA256withRSA");
+
+			// Assinatura usa chave privada para assinar
+			s.initVerify(AssymKeyHandler.getPublicKey("keystore.chaves", "password", "chaves"));
+
+			long read = 0;
+			byte[] buffer = new byte[1024];
+			while (read >= 0) {
+				read = fis.read(buffer, 0, buffer.length);
+				if (read > -1) {
+					s.update(buffer, 0, (int) read);
+				}
+			}
+
+			return s.verify(signature);
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		if (args.length == 0) {
-			System.out.println("Assina ou Verifica ficheiros.\n\nuso:\n'Assina -a <nome_do_ficheiro>' para assinar.\n'Assina -v <nome_do_ficheiro>' para verificar.");
+			System.out.println(
+					"Assina ou Verifica ficheiros.\n\nuso:\n'Assina -a <nome_do_ficheiro>' para assinar.\n'Assina -v <nome_do_ficheiro>' para verificar.");
 			System.exit(1);
 		}
-		
+
 		int flag = args[0].contentEquals("-a") ? 0 : 1;
-		
+
 		File i = new File(input);
-		if(!i.exists()) {
+		if (!i.exists()) {
 			i.mkdirs();
 		}
-		
+
 		String filename = args[1];
 
 		switch (flag) {
 		case 0:
 			assina(filename);
+			System.out.println("Ficheiro assinado");
 			break;
 		case 1:
 			System.out.println(verifica(filename));
